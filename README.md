a824085667defd020e0929e3fe2f0ad2b1eab60b
Eric Saucedo <ericalexanders@gmail.com>
2025-01-31 18:34:49 -0300

    feat: README.md completed

---
+++ README.md
@@ -0,0 +1,78 @@
+# Mercado Libre Challange (BFF)
+
+### Node + Express + Typescript + Nodemon + Jest
+
+Se usa Biomejs para el linter y el formateo del código.
+Asi también vitest para los test unitarios.
+
+Copmandos:
+
+Modo desarrollo: `npm run dev`
+
+Correr tests: `npm run test`
+
+## Endpoints
+
+> ### Obtener items
+
+- **URL:** `/api/items`
+- **Método:** `GET`
+- **Descripción:** Obtiene una lista de items de Mercado Libre.
+- **Parámetros de consulta:** Los parámetros de consulta se pasan directamente a la API de Mercado Libre.
+- **Respuesta exitosa:**
+  ```json
+  {
+    "author": {
+      "name": "Eric",
+      "lastname": "Saucedo"
+    },
+    "categories": ["category1", "category2"],
+    "items": [
+      {
+        "id": "item_id",
+        "title": "item_title",
+        "price": {
+          "currency": "currency_id",
+          "amount": 100,
+          "decimals": 0
+        },
+        "picture": "thumbnail_url",
+        "condition": "new",
+        "free_shipping": true
+      }
+    ]
+  }
+
+> ### Obtener detalles de un item
+- **URL:** `/api/items/:id`
+- **Método:** `GET`
+- **Descripción:** Obtiene los detalles de un item específico de Mercado Libre.
+- **Parámetros de ruta:**
+  - id (string): El ID del item.
+
+  ```json
+  {
+    "author": {
+      "name": "Eric",
+      "lastname": "Saucedo"
+    },
+    "item": {
+      "id": "item_id",
+      "title": "item_title",
+      "price": {
+        "currency": "currency_id",
+        "amount": 100,
+        "decimals": 0
+      },
+      "pictures": [
+        {
+          "secure_url": "picture_url"
+        }
+      ],
+      "condition": "new",
+      "free_shipping": true,
+      "sold_quantity": 10,
+      "description": "item_description"
+    }
+  }
+  ```
--- src/controllers/itemsControllers.ts
+++ src/controllers/itemsControllers.ts
@@ -1,32 +1,38 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
 import axios from "axios";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import { transformItemDetailResponse, transformResponse } from "../utils";
 
 dotenv.config();
 
-const URL_QUERY = process.env.API_MELI_QUERY as string
-const URL_ITEMS = process.env.API_MELI_ITEMS as string
+const URL_QUERY = process.env.API_MELI_QUERY as string;
+const URL_ITEMS = process.env.API_MELI_ITEMS as string;
 
-export const getItems: (req: Request, res: Response) => Promise<any> =  async (req, res) => {
-  try {
-    const { data } = await axios.get(URL_QUERY, {
-      params: req.query
-    });
-    return res.json(transformResponse(data));
-  } catch (error) {
-    console.log("Error", error)
-    return res.status(500).json({ error: 'Internal server error' });
-  }
-}
+export const getItems: (req: Request, res: Response) => Promise<any> = async (
+	req,
+	res,
+) => {
+	try {
+		const { data } = await axios.get(URL_QUERY, {
+			params: req.query,
+		});
+		return res.json(transformResponse(data));
+	} catch (error) {
+		console.log("Error", error);
+		return res.status(500).json({ error: "Internal server error" });
+	}
+};
 
-export const getItemDetails: (req: Request, res: Response) => Promise<any> = async (req, res) => {
-  try {
-    const { data } = await axios.get(`${URL_ITEMS}/${req.params.id}`);
-    const { data: descData } = await axios.get(`${URL_ITEMS}/${req.params.id}/description`);
-    return res.json(transformItemDetailResponse(data, descData.plain_text));
-  } catch (error) {
-    console.log("Error", error)
-    return res.status(500).json({ error: 'Internal server error' });
-  }
-}
+export const getItemDetails: (req: Request, res: Response) => Promise<any> =
+	async (req, res) => {
+		try {
+			const { data } = await axios.get(`${URL_ITEMS}/${req.params.id}`);
+			const { data: descData } = await axios.get(
+				`${URL_ITEMS}/${req.params.id}/description`,
+			);
+			return res.json(transformItemDetailResponse(data, descData.plain_text));
+		} catch (error) {
+			console.log("Error", error);
+			return res.status(500).json({ error: "Internal server error" });
+		}
+	};
